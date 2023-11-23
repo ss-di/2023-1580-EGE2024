32b19e765fe39e5b354a6fa508016fd06d9e3899
Alexander Lobachev <ss_di@mail.ru>
2023-11-23 18:33:06 +0300

    Update 17_1970.py

--- 1580-2023-Т-319/2023_11_23 - ЕГЭ17/17_1970.py
+++ 1580-2023-Т-319/2023_11_23 - ЕГЭ17/17_1970.py
@@ -1,3 +1,34 @@
+def is_simple(x):
+    x = abs(x)
+    cnt = 0
+    for i in range(1, x+1):
+        if x % i == 0:
+            cnt += 1
+    return cnt==2
+
+def is_simple2(x):
+    x = abs(x)
+    if x == 1:
+        return False
+    cnt = 0
+    for i in range(2, x//2+1):
+        if x % i == 0:
+            cnt += 1
+    return cnt==0
+
+def is_simple3(x):
+    x = abs(x)
+    if x == 1:
+        return False
+    cnt = 0
+    for i in range(2, int(x**0.5)+1):
+        if x % i == 0:
+            cnt += 1
+    return cnt==0
+
+for i in range(1, 100):
+    if is_simple3(i):
+        print(i)
 
 # Прочитаем все строки из файла
 data = open('17_1970.txt').readlines()
@@ -13,7 +44,7 @@ cnt = 0 # Количество пар
 mx = -20001 # Максимальная сумма
 
 for i in range(len(data)-1): # -1 для того, чтобы не выйти за границу массива
-    if data[i] % 3 == 0 or data[i+1] % 3 == 0:
+    if is_simple(data[i]) or is_simple(data[i+1]):
         cnt += 1
         mx = max(mx, data[i] +  data[i+1])
         
